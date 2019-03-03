35ea576e478929d3fe5651efa83ec9ac721de3b3
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-03 01:20:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,14 +2,26 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  for name in array do 
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
+
 end
 
-def assign_rooms
-  
+def assign_rooms(attendees)
+  room=0
+  attendees.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
 end
 
-def printer
-  
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
 end
