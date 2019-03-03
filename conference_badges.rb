1241b9f5c4e13c531965d5c834abe38c5c67acaf
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-03 01:19:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,9 @@
-def badge_maker
-  
+def badge_maker(name)
+  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-  
+def batch_badge_creator(array)
+  for name in array do 
 end
 
 def assign_rooms
